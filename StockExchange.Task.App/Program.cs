14b019ed1f23b5da2c77f0934cc3e6202e60c128
Bartosz Woźniak <bartosz.wozniak.94@gmail.com>
2016-09-20 16:17:27 +0000

    Configure Log4Net
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange;C248

--- StockExchange.Task.App/Program.cs
+++ StockExchange.Task.App/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace StockExchange.Task.App
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            GlobalContext.Properties["LogName"] = args.Length > 0 ? args[0] : "";
+            var logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            logger.Debug("Job started");
+            logger.Debug("Parameters: " + string.Join(" ", args));
+            Console.ReadLine();
         }
     }
 }
