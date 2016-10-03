29de491f1cd440449d2d05a42e7c326aee6a55d0
Marcin Chudy <marcinchudy94@hotmail.com>
2016-10-03 19:39:01 +0000

    Install packages for unit testing
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange;C264

--- StockExchange.UnitTest/UnitTest1.cs
+++ StockExchange.UnitTest/UnitTest1.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Xunit;
 
 namespace StockExchange.UnitTest
 {
-    [TestClass]
     public sealed class UnitTest1
     {
-        [TestMethod]
+        [Fact]
         public void TestMethod1()
         {
         }
