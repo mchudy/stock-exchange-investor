f80b4f2c57bb061d2d9189ecc0d66fc9ad0fefb8
Marcin Chudy <marcinchudy94@hotmail.com>
2016-10-31 16:15:50 +0000

    Install bootflat and add hamburger button
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C315

---
+++ StockExchange.Web/scripts/site.js
@@ -0,0 +1,9 @@
+﻿(function() {
+    'use strict';
+
+    $('.sidebar-toggle').click(function() {
+        $('.sidebar').toggleClass('hide-sidebar');
+        $('.main-content').toggleClass('hide-sidebar');
+    });
+
+})();
