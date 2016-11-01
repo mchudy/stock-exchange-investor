ff7bf38727f3ce95d9c6a314dd819d61d8af139d
Marcin Chudy <marcinchudy94@hotmail.com>
2016-11-01 17:38:28 +0000

    Install highstock and add mock chart (no real data yet)
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C320

---
+++ StockExchange.Web/scripts/charts.js
@@ -0,0 +1,24 @@
+﻿(function() {
+    'use strict';
+
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+        Highcharts.stockChart('chart-container', {
+            rangeSelector: {
+                selected: 1
+            },
+
+            title: {
+                text: 'AAPL Stock Price'
+            },
+
+            series: [{
+                name: 'AAPL',
+                data: data,
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }]
+        });
+    });
+
+})();
