dbc36ed4b584c0f646cde0531ea1ef416742fc8c
Mateusz Bąkała <bakala12@windowslive.com>
2016-12-06 17:33:36 +0000

    Adding indicator type enum
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C383

---
+++ StockExchange.Business/Indicators/IndicatorType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchange.Business.Indicators
+{
+    public enum IndicatorType : byte
+    {
+        Atr = 1,
+        Macd = 2,
+        Obv = 3,
+        Roc = 4,
+        Rsi = 5
+    }
+}
