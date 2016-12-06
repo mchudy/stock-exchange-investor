eb302671b44bb8b4ed134784d7d456706bf2c927
Mateusz Bąkała <bakala12@windowslive.com>
2016-12-06 19:22:01 +0000

    Indicator factory
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C389

---
+++ StockExchange.Business/Indicators/IndicatorFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchange.Business.Indicators
+{
+    public class IndicatorFactory
+    {
+        private static readonly string IndicatorSuffix = "Indicator";
+
+        public IIndicator CreateIndicator(IndicatorType indicatorType)
+        {
+            var typeName =
+                this.GetType()
+                    .Assembly.GetTypes()
+                    .Where(t => t.GetInterfaces().Contains(typeof(IIndicator)))
+                    .FirstOrDefault(t => t.Name.Equals(indicatorType + IndicatorSuffix));
+            if (typeName == null) return null;
+            IIndicator indicator = null;
+            try
+            {
+                indicator = Activator.CreateInstance(typeName) as IIndicator;
+            }
+            catch
+            {
+                // ignored
+            }
+            return indicator;
+        }
+    }
+}
---
+++ StockExchange.UnitTest/Indicators/IndicatorFactoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using StockExchange.Business.Indicators;
+using Xunit;
+
+namespace StockExchange.UnitTest.Indicators
+{
+    public class IndicatorFactoryTests
+    {
+        private readonly IndicatorFactory _factory = new IndicatorFactory();
+
+        [Theory]
+        [InlineData(IndicatorType.Macd, typeof(MacdIndicator))]
+        [InlineData(IndicatorType.Rsi, typeof(RsiIndicator))]
+        [InlineData(IndicatorType.Roc, typeof(RocIndicator))]
+        [InlineData(IndicatorType.Obv, typeof(ObvIndicator))]
+        [InlineData(IndicatorType.Atr, typeof(AtrIndicator))]
+        public void Is_Indicator_Factory_Create_Correct_Instance(IndicatorType type, Type resultType)
+        {
+            var indicator = _factory.CreateIndicator(type);
+            indicator.Should().NotBeNull();
+            indicator.GetType().Should().Be(resultType);
+        }
+    }
+}
