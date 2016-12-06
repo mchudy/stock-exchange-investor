be5e05f31ae6548d51312cf646178a02abca8229
Mateusz Bąkała <bakala12@windowslive.com>
2016-12-06 18:38:10 +0000

    Getting indicator's properties via reflection
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C384

---
+++ StockExchange.Business/Indicators/IndicatorProperty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchange.Business.Indicators
+{
+    public class IndicatorProperty
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class IndicatorPropertyList
+    {
+        private static readonly string IndicatorSuffix = "Indicator";
+
+        public IndicatorType IndicatorType { get; set; }
+        public List<IndicatorProperty> Properties { get; set; }
+
+        public IndicatorPropertyList(IndicatorType indicatorType)
+        {
+            IndicatorType = indicatorType;
+            Properties = new List<IndicatorProperty>();
+            string indicator = indicatorType + IndicatorSuffix;
+            var type = this.GetType().Assembly.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(IIndicator))).
+                FirstOrDefault(t => t.Name.Equals(indicator));
+            if (type == null) return;
+            foreach (var property in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                Properties.Add(new IndicatorProperty() { Name = property.Name });
+            }
+        }
+    }
+}
