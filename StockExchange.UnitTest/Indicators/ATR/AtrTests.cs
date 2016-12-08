0a69567700d3eafa8a0428f2f48e2fc9d57831f5
Marcin Chudy <marcinchudy94@hotmail.com>
2016-12-08 19:09:33 +0000

    Fix ATR test
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C409

--- StockExchange.UnitTest/Indicators/ATR/AtrTests.cs
+++ StockExchange.UnitTest/Indicators/ATR/AtrTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using StockExchange.Business.Indicators;
 using StockExchange.DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -21,10 +22,11 @@ namespace StockExchange.UnitTest.Indicators.ATR
         {
             // RS values for the prices are 2 and 6 (high - low), so the result should be 4
             // (for the first day we take normal average)
+            var date = new DateTime(2016, 10, 1);
             var prices = new List<Price>
             {
-                new Price {HighPrice = 4, LowPrice = 2, ClosePrice = 5},
-                new Price {HighPrice = 8, LowPrice = 2, ClosePrice = 4}
+                new Price {HighPrice = 4, LowPrice = 2, ClosePrice = 5, Date = date},
+                new Price {HighPrice = 8, LowPrice = 2, ClosePrice = 4, Date = date.AddDays(1)}
             };
             _indicator.Term = 2;
 
