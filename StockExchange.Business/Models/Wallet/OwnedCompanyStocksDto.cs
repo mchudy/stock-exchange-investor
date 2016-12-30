348a0ec937a08255dee5a857dad01f6ae704e220
Bartosz Woźniak <bartosz.wozniak.94@gmail.com>
2016-12-30 17:31:57 +0000

    Update OwnedCompanyStocksDto Model
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C517

--- StockExchange.Business/Models/Wallet/OwnedCompanyStocksDto.cs
+++ StockExchange.Business/Models/Wallet/OwnedCompanyStocksDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using StockExchange.Common;
 
 namespace StockExchange.Business.Models.Wallet
 {
@@ -12,21 +13,25 @@ namespace StockExchange.Business.Models.Wallet
         public string CompanyName { get; set; }
 
         [Display(Name = "Quantity")]
+        [DisplayFormat(DataFormatString = Consts.Formats.Integer)]
         public int OwnedStocksCount { get; set; }
 
         /// <summary>
         /// Current price for a single stock
         /// </summary>
         [Display(Name = "Current Price")]
+        [DisplayFormat(DataFormatString = Consts.Formats.Currency)]
         public decimal CurrentPrice { get; set; }
 
         /// <summary>
         /// Represents total money put into stocks of the company
         /// </summary>
         [Display(Name = "Total Buy Price")]
+        [DisplayFormat(DataFormatString = Consts.Formats.Currency)]
         public decimal TotalBuyPrice { get; set; }
 
         [Display(Name = "Buy Price")]
+        [DisplayFormat(DataFormatString = Consts.Formats.Currency)]
         public decimal AverageBuyPrice { get; set; }
 
         /// <summary>
@@ -34,8 +39,11 @@ namespace StockExchange.Business.Models.Wallet
         /// the current prices)
         /// </summary>
         [Display(Name = "Total Current Value")]
+        [DisplayFormat(DataFormatString = Consts.Formats.Currency)]
         public decimal CurrentValue { get; set; }
 
+        [Display(Name = "Profit")]
+        [DisplayFormat(DataFormatString = Consts.Formats.Currency)]
         public decimal Profit => CurrentValue - TotalBuyPrice;
     }
 }
