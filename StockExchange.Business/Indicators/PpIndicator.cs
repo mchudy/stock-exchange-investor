61d1df39a109536bcbc557a5e8b328ebda85cc00
Mateusz Bąkała <bakala12@windowslive.com>
2016-12-30 19:54:15 +0000

    Pivot point signals
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C521

--- StockExchange.Business/Indicators/PpIndicator.cs
+++ StockExchange.Business/Indicators/PpIndicator.cs
@@ -1,7 +1,9 @@
-﻿using StockExchange.Business.Models.Indicators;
+﻿using System;
+using StockExchange.Business.Models.Indicators;
 using StockExchange.DataAccess.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using StockExchange.Business.Indicators.Common;
 
 namespace StockExchange.Business.Indicators
@@ -21,7 +23,60 @@ namespace StockExchange.Business.Indicators
         public IList<Signal> GenerateSignals(IList<Price> prices)
         {
             List<Signal> signals = new List<Signal>();
+            var values = CalculateSupportsAndResistances(prices);
+            PivotPointSupportResistance lastpp = null;
+            SignalAction lastAction = SignalAction.NoSignal;
+            foreach (var pp in values)
+            {
+                if (lastpp == null)
+                {
+                    lastpp = pp;
+                    continue;
+                }
+                if(pp.ClosePrice > lastpp.Resistance2 && lastAction != SignalAction.Buy)
+                    signals.Add(new Signal(SignalAction.Buy) {Date = pp.Date});
+                if(pp.ClosePrice < lastpp.Support2 && lastAction != SignalAction.Sell)
+                    signals.Add(new Signal(SignalAction.Sell) {Date = pp.Date});
+                lastpp = pp;
+            }
             return signals;
+        }
+
+        private IList<PivotPointSupportResistance> CalculateSupportsAndResistances(IList<Price> prices)
+        {
+            return prices.Select(p => new PivotPointSupportResistance(p)).ToList();
+        } 
+    }
+
+    internal class PivotPointSupportResistance
+    {
+        private readonly Price _price;
+        public DateTime Date => _price.Date;
+        public decimal PivotPoint => (_price.LowPrice + _price.HighPrice + _price.ClosePrice)/3;
+        public decimal Resistance1 => 2*PivotPoint - _price.LowPrice;
+        public decimal Resistance2 => PivotPoint + _price.HighPrice - _price.LowPrice;
+        public decimal Support1 => 2*PivotPoint - _price.HighPrice;
+        public decimal Support2 => PivotPoint - _price.HighPrice + _price.LowPrice;
+        public decimal OpenPrice => _price.OpenPrice;
+        public decimal ClosePrice => _price.ClosePrice;
+        public decimal HightPrice => _price.HighPrice;
+        public decimal LowPrice => _price.LowPrice;
+
+        public decimal TomorrowHigh => GetX()/2 - _price.LowPrice;
+        public decimal TomorrowLow => GetX()/2 - _price.HighPrice;
+
+        private decimal GetX()
+        {
+            if (ClosePrice > OpenPrice)
+                return ClosePrice + LowPrice + 2 * HightPrice;
+            if (ClosePrice < OpenPrice)
+                return HightPrice + ClosePrice + 2 * LowPrice;
+            return HightPrice + LowPrice + 2 * ClosePrice;
+        }
+
+        public PivotPointSupportResistance(Price price)
+        {
+            _price = price;
         }
     }
 }
