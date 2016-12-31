b7f97ff4303f56deca420be2582c1a932195a829
Mateusz Bąkała <bakala12@windowslive.com>
2016-12-31 08:20:42 +0000

    Vpt indicator signals
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C533

--- StockExchange.Business/Indicators/VptIndicator.cs
+++ StockExchange.Business/Indicators/VptIndicator.cs
@@ -25,6 +25,29 @@ namespace StockExchange.Business.Indicators
         public IList<Signal> GenerateSignals(IList<Price> prices)
         {
             var signals = new List<Signal>();
+            var values = Calculate(prices);
+            int priceTerm = 14;
+            var priceTrend = MovingAverageHelper.ExpotentialMovingAverage(prices, priceTerm);
+            SignalAction lastAction = SignalAction.NoSignal;
+            for (int i = priceTerm; i < prices.Count; i++)
+            {
+                if(values[i-1].Value < values[i].Value && priceTrend[i-priceTerm].Value < priceTrend[i - priceTerm + 1].Value)
+                {
+                    if (lastAction != SignalAction.Buy)
+                    {
+                        signals.Add(new Signal(SignalAction.Buy) {Date = values[i].Date});
+                        lastAction = SignalAction.Buy;
+                    } 
+                }
+                if(values[i-1].Value > values[i].Value && priceTrend[i-priceTerm].Value > priceTrend[i - priceTerm + 1].Value)
+                {
+                    if (lastAction != SignalAction.Sell)
+                    {
+                        signals.Add(new Signal(SignalAction.Sell) {Date = values[i].Date});
+                        lastAction = SignalAction.Sell;
+                    }
+                }
+            }
             return signals;
         }
     }
