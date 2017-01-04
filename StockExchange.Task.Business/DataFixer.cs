2fc08cadfbadad182ca4ab71f46be944699b7b11
Marcin Chudy <marcinchudy94@hotmail.com>
2017-01-04 20:25:47 +0000

    Build fix
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C577

--- StockExchange.Task.Business/DataFixer.cs
+++ StockExchange.Task.Business/DataFixer.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Linq.Dynamic;
 using System.Reflection;
 
 namespace StockExchange.Task.Business
