592f1b9136b12558c9d421e74a9ca3200851e7e5
Mateusz Bąkała <bakala12@windowslive.com>
2017-01-06 20:40:05 +0000

    RSI signals fixed
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C599

--- StockExchange.Business/Indicators/RsiIndicator.cs
+++ StockExchange.Business/Indicators/RsiIndicator.cs
@@ -69,12 +69,12 @@ namespace StockExchange.Business.Indicators
             SignalAction previousAction = SignalAction.NoSignal;
             foreach (var indicatorValue in values)
             {
-                if (indicatorValue.Value < Minimum && previousAction != SignalAction.Sell)
+                if (indicatorValue.Value > Maximum && previousAction != SignalAction.Sell)
                 {
                     signals.Add(new Signal(SignalAction.Sell) { Date = indicatorValue.Date });
                     previousAction = SignalAction.Sell;
                 }
-                else if (indicatorValue.Value > Maximum && previousAction != SignalAction.Buy)
+                else if (indicatorValue.Value < Minimum && previousAction != SignalAction.Buy)
                 {
                     signals.Add(new Signal(SignalAction.Buy) {Date = indicatorValue.Date});
                     previousAction = SignalAction.Buy;
