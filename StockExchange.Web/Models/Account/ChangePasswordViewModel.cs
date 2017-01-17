5fec3c40db43799cbd657fa83642b9dc081f1ec9
Marcin Chudy <marcinchudy94@hotmail.com>
2017-01-17 18:34:20 +0000

    Add form for changing password
    git-tfs-id: [https://bartosz-wozniak.visualstudio.com/DefaultCollection]$/StockExchange/StockExchange_Dev;C658

---
+++ StockExchange.Web/Models/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockExchange.Web.Models.Account
+{
+    /// <summary>
+    /// View model for changing password form
+    /// </summary>
+    public class ChangePasswordViewModel
+    {
+        /// <summary>
+        /// Old password
+        /// </summary>
+        [Required]
+        [Display(Name = "Old password")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+        /// <summary>
+        /// New password
+        /// </summary>
+        [Required]
+        [Display(Name = "New password")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        /// <summary>
+        /// Confirm new password
+        /// </summary>
+        [Required]
+        [Display(Name = "Confirm new password")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
